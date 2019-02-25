5ae1087618e362b2406ed278382306b22224469d
Daniel Innes <Daniel.W.Innes@gmail.com>
2019-02-25 13:40:58 -0500

    Client Program

---
+++ Assignment 3/src/Assignment3.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Assignment3 {
+    public static void main(String [] args) {
+        BufferedReader keyboardReader = new BufferedReader(new InputStreamReader(System.in));
+        String input = new String();
+        try {
+            System.out.print("Enter the G-Language word to cheack:");
+            input = keyboardReader.readLine();
+        }catch (IOException e){
+            System.out.println(e);
+        }
+        LanguageRecognizerG w1 = new LanguageRecognizerG(input);
+        w1.recursivePrintG();
+        try {
+            System.out.print("Enter the infix expression to evaluate:");
+            input = keyboardReader.readLine();
+        }catch (IOException e){
+            System.out.println(e);
+        }
+        InfixCalculator w2 = new InfixCalculator(input);
+        InfixCalculator.evaluateInfix;
+    }
+
+}
